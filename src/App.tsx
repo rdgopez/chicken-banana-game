c76d8da54d389552c8a333590c195985e5f76cac
rdgopez <rdgopez@student.hau.edu.ph>
2025-07-07 17:03:08 +0800

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, type ChangeEvent } from "react";
+import { useEffect, useState, type ChangeEvent } from "react";
 
 type Role = "chicken" | "banana";
 
@@ -17,6 +17,7 @@ function App() {
     role: "banana",
   });
   const [ready, setReady] = useState<boolean>(false);
+  const [startAs, setStartAs] = useState<Player>(player1);
 
   function createInputChangeName(
     player: Player,
@@ -36,21 +37,25 @@ function App() {
         <div className="flex justify-between gap-5">
           <div className="flex items-center gap-3">
             &#x1F414;{" "}
-            <input value={player1.name} disabled={ready} onChange={createInputChangeName(player1, setPlayer1)} className="border text-xl disabled:border-none border-neutral-200 rounded-full px-3 py-2" type="text" />
+            <input value={player1.name} disabled={ready} onChange={createInputChangeName(player1, setPlayer1)} className="border text-xl disabled:border-none border-neutral-300 rounded-full px-3 py-2" type="text" />
           </div>
 
           <div className="flex items-center gap-3">
             &#x1F34C;{" "}
-            <input value={player2.name} disabled={ready} onChange={createInputChangeName(player2, setPlayer2)} className="border text-xl disabled:border-none border-neutral-200 rounded-full px-3 py-2" type="text" />
+            <input value={player2.name} disabled={ready} onChange={createInputChangeName(player2, setPlayer2)} className="border text-xl disabled:border-none border-neutral-300 rounded-full px-3 py-2" type="text" />
           </div>
         </div>
         
-        { !ready && <button onClick={() => setReady(true)} className="rounded-full w-full px-3 py-2 border border-neutral-200 cursor-pointer hover:bg-neutral-100">
-          Start!
+        { !ready && <button onClick={() => { setReady(true); setStartAs(player1); }} className="rounded-full w-full px-3 py-2 border border-neutral-300 cursor-pointer hover:bg-neutral-100">
+          Start as {getEmojiRole("chicken")} Chicken! 
+        </button> }
+
+        { !ready && <button onClick={() => { setReady(true); setStartAs(player2); }} className="rounded-full w-full px-3 py-2 border border-neutral-300 cursor-pointer hover:bg-neutral-100">
+          Start as {getEmojiRole("banana")} Banana! 
         </button> }
       </div>
 
-      {ready && <Game onClickPlayAgain={() => setReady(false)} player1={player1} player2={player2} />}
+      {ready && startAs && <Game onClickPlayAgain={() => setReady(false)} player1={player1} player2={player2} startAs={startAs} />}
     </div>
   );
 }
@@ -58,15 +63,17 @@ function App() {
 interface GameProps {
   player1: Player;
   player2: Player;
+  startAs: Player;
   onClickPlayAgain: () => void;
 }
 
-function createFairState(size: number): { isRevealed: boolean, value: Role }[] {
+function createFairState(size: number): { isRevealed: boolean, isTransparentRevealed: boolean, value: Role }[] {
   let chickenLeft = size * 0.5;
   let bananaLeft = size * 0.5;
 
   return [...Array(size)].map(_ => ({ 
       isRevealed: false, 
+      isTransparentRevealed: false,
       value: Math.random() > 0.5 ? 
         (chickenLeft-- > 0 ? "chicken" : "banana") : 
         (bananaLeft-- > 0 ? "banana" : "chicken")
@@ -74,24 +81,31 @@ function createFairState(size: number): { isRevealed: boolean, value: Role }[] {
   )
 }
 
-function Game({ player1, player2, onClickPlayAgain }: GameProps) {
-  const [currentTurn, setCurrentTurn] = useState<Player>(player1);
-  const [gridState, setGridState] = useState<{ isRevealed: boolean, value: Role }[]>(createFairState(36));
+function getEmojiRole(role: Role) {
+  return role === "chicken" ? <>&#x1F414;</>: <>&#x1F34C;</>
+}
+
+function Game({ player1, player2, startAs, onClickPlayAgain }: GameProps) {
+  const [currentTurn, _] = useState<Player>(startAs);
+  const [gridState, setGridState] = useState<{ isRevealed: boolean, isTransparentRevealed: boolean, value: Role }[]>(createFairState(36));
   const [loser, setLoser] = useState<Player | null>(null);
+
+  const [hover, setHover] = useState<Role | null>(null);
 
   const winner = loser === player1 ? player2 : player1;
   const chickenLeft = gridState.reduce((previous, current) => previous + (!current.isRevealed && current.value === "chicken" ? 1 : 0), 0);
   const bananaLeft = gridState.reduce((previous, current) => previous + (!current.isRevealed && current.value === "banana" ? 1 : 0), 0);
 
-  function getEmojiRole(role: Role) {
-    return role === "chicken" ? <>&#x1F414;</>: <>&#x1F34C;</>
-  }
+  useEffect(() => {
+    if (loser) setGridState(gridState.map(state => ({ ...state, isTransparentRevealed: state.isRevealed ? false : true })));
+  }, [ loser, gridState ]);
 
   const grid = [...Array(36)].map((_, index) => {
-    const { isRevealed, value } = gridState[index];
+    const { isRevealed, isTransparentRevealed, value } = gridState[index];
     
     function handleClick() {
-      setCurrentTurn(currentTurn === player1 ? player2 : player1);
+      if (loser) return;
+      // setCurrentTurn(currentTurn === player1 ? player2 : player1);
       
       setGridState(gridState.map((gridItem, gridIndex) => {
           if (gridIndex !== index) return gridItem;
@@ -101,26 +115,29 @@ function Game({ player1, player2, onClickPlayAgain }: GameProps) {
           }
 
           if ((currentTurn.role === "chicken" && chickenLeft-1 === 0) || (currentTurn.role === "banana" && bananaLeft-1 === 0)) {
-            setLoser(currentTurn === player1 ? player2 : player1)
+            setLoser(currentTurn === player1 ? player2 : player1);
           }
 
           return { ...gridItem, isRevealed: true };
         })
       )
+
+      
     }
 
     return (
-      <div key={index} onClick={handleClick} className={`border border-neutral-300 cursor-pointer flex-[1_0_15%] flex items-center justify-center text-xl ${isRevealed ? "" : "hover:bg-neutral-200"}`}>
-        { isRevealed ? getEmojiRole(value) : (index+1) }
+      <div key={index} onMouseEnter={_ => setHover(value)} onClick={handleClick} className={`border border-neutral-400 cursor-pointer flex-[1_0_15%] flex items-center justify-center text-2xl ${isRevealed ? "" : "hover:bg-neutral-200"} ${isTransparentRevealed ? value === "chicken" ? "bg-blue-100" : "bg-yellow-100" : "opacity-100"}`}>
+        { (isRevealed || isTransparentRevealed) ? getEmojiRole(value) : (index+1) }
       </div>
     );
   });
 
   return (
     <div className="mx-auto">
+      <div className={`fixed bottom-0 left-0 w-[5px] h-[5px] ${hover === "chicken" ? "bg-neutral-100" : "bg-neutral-200"}`}></div>
       { loser && (
-          <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-neutral-100/50">
-            <div className="bg-neutral-200 w-1/2 h-1/2 flex flex-col gap-5 justify-center items-center rounded-xl text-xl">
+          <div className="flex items-center justify-center">
+            <div className="bg-neutral-200 w-1/2 h-1/2 flex flex-col gap-5 p-5 justify-center items-center rounded-xl text-xl">
               <div>{loser.name} {getEmojiRole(loser.role)} Lost!</div>
               <div>And {winner.name} {getEmojiRole(winner.role)} Won!</div>
               <button className="p-2 rounded-xl bg-red-500 text-white cursor-pointer hover:bg-red-600" onClick={onClickPlayAgain}>Play again</button>
@@ -130,8 +147,8 @@ function Game({ player1, player2, onClickPlayAgain }: GameProps) {
       }
 
       <div className="flex flex-col p-2 text-lg">
-        <div>{chickenLeft} {getEmojiRole("chicken")} left!</div>
-        <div>{bananaLeft} {getEmojiRole("banana")} left!</div>
+        <div>{Math.round(chickenLeft/36 * 100)}% {getEmojiRole("chicken")} left!</div>
+        <div>{Math.round(bananaLeft/36 * 100)}% {getEmojiRole("banana")} left!</div>
         <div>Turn of {currentTurn.name} {getEmojiRole(currentTurn.role)}</div>
       </div>
       <div className="w-[600px] h-[600px]  flex flex-wrap">{grid}</div>
